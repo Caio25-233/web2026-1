d282fc200b0ba1a77b80d9daf04c31d52f416019
Caio25-233 <zm25223@josai.ac.jp>
2026-06-29 11:33:49 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,11 +1,21 @@
 alert('Hello, Caio!');
+
 alert(1 + 1); 
+
 alert(Math.sqrt(1 + 1));
+
 alert(Math.sqrt(2) + Math.sqrt(3));
+
 alert(1 / 0); 
+
 alert(0 / 0);
+
 alert(2 + 3);
+
 alert('2' + '3')
+
 alert(number('2') + ('3'));
+
 alert('2' + 3);
+
 alert(number('2') + 3;
